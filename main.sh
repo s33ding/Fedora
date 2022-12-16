263caa3d9c8dfe077655070ffd6e7910a234f02b
s33ding <robertomdiniz@protonmail.com>
2022-12-16 12:05:12 -0300

    saving

--- main.sh
+++ main.sh
@@ -1,12 +1,12 @@
 sudo dnf update -y
+sudo bash docker_install.sh 
 sudo bash dnf_install.sh
 sudo bash vscode_install.sh
 sudo bash psql.sh
 sudo bash pgadmin_install.sh
-sudo bash docker_install.sh 
 sudo bash brave_install.sh
 sudo bash AWS-SAM.sh
 sudo bash install flatpaks_install.sh
 sudo bash brew_install.sh
-sudo bash snap_install.sh
 bash pip_install.sh
+sudo bash snap_install.sh
