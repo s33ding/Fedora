c9ddea8aefb3cdcbbd061b7ad43c25c2ab42223b
s33ding <robertomdiniz@protonmail.com>
2023-04-21 12:45:35 -0300

    saving

---
+++ terraform_install.sh
@@ -0,0 +1,5 @@
+#src: https://developer.hashicorp.com/terraform/downloads
+
+sudo dnf install -y dnf-plugins-core
+sudo dnf config-manager --add-repo https://rpm.releases.hashicorp.com/fedora/hashicorp.repo
+sudo dnf -y install terraform
