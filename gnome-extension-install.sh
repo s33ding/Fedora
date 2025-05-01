fa72f72a6a2bd4d4fd8192c0256a84c1ae45e54a
s33ding <robertomdiniz@protonmail.com>
2025-04-30 23:54:17 -0300

    saving

---
+++ gnome-extension-install.sh
@@ -0,0 +1,1 @@
+sudo dnf install gnome-extensions-app.x86_64
--- terraform_install.sh
+++
@@ -1,5 +0,0 @@
-#src: https://developer.hashicorp.com/terraform/downloads
-
-sudo dnf install -y dnf-plugins-core
-sudo dnf config-manager --add-repo https://rpm.releases.hashicorp.com/fedora/hashicorp.repo
-sudo dnf -y install terraform
