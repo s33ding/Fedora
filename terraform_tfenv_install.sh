237804d352f64041d9597ccca81de2531a41dada
s33ding <robertomdiniz@protonmail.com>
2026-03-28 19:54:52 -0300

    update terraform install script to use latest version

--- terraform_tfenv_install.sh
+++ terraform_tfenv_install.sh
@@ -3,7 +3,10 @@
 # Install tfenv
 git clone https://github.com/tfutils/tfenv.git ~/.tfenv
 echo 'export PATH="$HOME/.tfenv/bin:$PATH"' >> ~/.bashrc
-source ~/.bashrc
+export PATH="$HOME/.tfenv/bin:$PATH"
 
-# Install Terraform
-tfenv install 0.15.5
+# Install latest Terraform
+tfenv install latest
+tfenv use latest
+
+terraform --version
